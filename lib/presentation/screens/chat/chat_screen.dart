cdd6a6b75d1fec54b0047454a716f1112ab8ce8c
jonagarme <garciajonathan268@gmail.com>
2023-04-27 12:58:45 -0500

    app subida, falta textform

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://ichef.bbci.co.uk/news/640/cpsprodpb/17F65/production/_127594189_gettyimages-1427974229.jpg'),
+          ),
+        ),
+        title: const Text('Mi amor ❤'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return (index % 2 == 0)
+                      ? const HerMessageBubble()
+                      : const MyMessageBubble();
+                },
+              ),
+            ),
+            const Text('Hola')
+          ],
+        ),
+      ),
+    );
+  }
+}
